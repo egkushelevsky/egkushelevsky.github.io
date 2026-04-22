bb8e1645cfc973c40db75db064cfa39331a181c4
egkushelevsky <164430094+egkushelevsky@users.noreply.github.com>
2026-04-22 14:57:35 -0400

    Resume

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,8 @@
+import resume from "../assets/documents/Elizabeth_Kushelevsky_Resume.pdf"
+
+export const Resume = () => {
+    return (
+        <object data={resume} width="100%" height="600px">
+        </object>
+    );
+  };
